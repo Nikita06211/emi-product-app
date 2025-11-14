4be2c7bbf6b2b7fabaf9904292d492b4903a84fd
Nikita Bansal <120494269+Nikita06211@users.noreply.github.com>
2025-11-14 21:15:34 +0530

    Update CORS origin to new frontend URL

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://emi.nikitabansal.xyz"],
+    origin: ["http://localhost:5173","https://emi-frontend-fawn.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
